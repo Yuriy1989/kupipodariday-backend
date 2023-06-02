fed85a4f38df298efdef3176e3fa1ba33af816e4
YuraNotebook <dudinyv@yandex.ru>
2023-06-02 22:01:31 +0300

    add: typeOPT

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'student',
+      password: 'student',
+      database: 'kupipodariday',
+      entities: [],
+      synchronize: true,
+    }),
+    UsersModule
+  ],
   controllers: [AppController],
   providers: [],
 })
